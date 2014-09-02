c69063751bfcb4d246457721bcd10c5372e639b8
caseylance <caseylance@gmail.com>
2014-09-02 04:41:53 -0600

    x

--- Project01/Project01/project1.cpp
+++ Project01/Project01/project1.cpp
@@ -22,10 +22,13 @@ int main()
 	//declarations
 	double taxableIncome = -1;
 	string filingStatus;
+	int statusIndicator = -1;
+	double taxOwed;
 	//const double taxOwed;
 	const double TAX_RATE_ONE = .023;
 	const double TAX_RATE_TWO = .033;
 	const double TAX_RATE_THREE = .052;
+	const double TAX_RATE_FOUR = .075;
 
 
 	const double SINGLE_LIMIT_ONE = 863;
@@ -38,45 +41,69 @@ int main()
 
 	//Prompt the user to enter in their taxable income.
 	cout << "/nPlease enter your taxable income.";
-	do
+
+
+
+	//Get the input.
+
+
+	while (taxableIncome < 0)
 	{
-		
-		//Get the input.
+
 		cin >> taxableIncome;
-		
-		//if (!(cin >> taxableIncome))
-		//{
-			//cout << "/nYou must enter a valid numerical amount.";
-			//taxableIncome = -1;
-			//cin.ignore();
-			
-		//}
-		if (!(cin >> taxableIncome) || taxableIncome < 0)
+		if (cin.fail())
 		{
-			cout << "/nPlease enter a valid numerical amount.";
-			cin.ignore();
+			cout << "/nYou must enter a valid numeric value.  Please try again.";
+			cin.clear();
+			cin.ignore(80, '\n');
+			taxableIncome = -1;
+		}
+		else if (taxableIncome < 0)
+		{
+
+			cout << "/nYou must enter a valid numberic value.  Please try again.";
+			cin.clear();
+			cin.ignore(80, '\n');
 			taxableIncome = -1;
 
 		}
-		//cin.ignore();
-	} while (taxableIncome < 0);
+	}
+
+	cout << taxableIncome;
+
 	//Validate the input to insure that it is a positive value.You should not assume that a numerical value is entered.If the user enters non - numerical data your program should give the user an error message.Re - do this step if the input is not valid.
-	 
-	
-		//Prompt the user to enter in "s" or "m" for filing single or jointly.
+
+
+	//Prompt the user to enter in "s" or "m" for filing single or jointly.
 	cout << "n/Are you filing as a single person or as a married person?  Please enter 'm' if filing as married and 's' if filing as single.";
-		//Get the input
-		cin >> filingStatus;
-		/*
-	//validate that it is either "s" or "m". Re-do this step if the input is not valid.
-	if (filingStatus != s || filingStatus != m)
+	//Get the input
+	while (statusIndicator == -1)
 	{
-		cout << "You did not enter a valid status.  Please enter 'm' if filing as married and 's' if filing as single.";
-			cin >> filingStatus;
+		cin >> filingStatus;
+		if (filingStatus == "s")
 		{
+			statusIndicator = 0;
+		}
+		
+		else if (filingStatus == "m")
+		{
+			statusIndicator = 1;
+		}
+		
+		else if (filingStatus != "m")
+		{
+			cout << "You must enter 's' or 'm.'  Please try again.";
+			cin.clear();
+			cin.ignore(80, '\n');
+		}
+	}
+
+
+
+		
 
 			//Based on the user's input, calculate the user's tax using the appropriate tax table and display the amount of money that person owes.
-			if (filingStatus = "s")
+			if (filingStatus == "s")
 			{
 				if (taxableIncome <= SINGLE_LIMIT_ONE)
 				{
@@ -86,10 +113,23 @@ int main()
 				{
 					taxOwed = ((taxableIncome - SINGLE_LIMIT_ONE) * TAX_RATE_TWO) + SINGLE_ADD_AMOUNT_TWO;
 				}
+				if (taxableIncome > SINGLE_LIMIT_TWO && taxableIncome <= SINGLE_LIMIT_THREE)
+				{
+					taxOwed = ((taxableIncome - SINGLE_LIMIT_TWO) * TAX_RATE_THREE) + SINGLE_ADD_AMOUNT_THREE;
+				}
+				if (taxableIncome > SINGLE_LIMIT_THREE)
+				{
+					taxOwed = ((taxableIncome - SINGLE_LIMIT_THREE) * TAX_RATE_FOUR) + SINGLE_ADD_AMOUNT_FOUR;
+				}
 			}
+			
+			cout << "\nYou owe $" << taxOwed << " in taxes.";
 
-
-			Present this data nicely formatted.
+			cout << "\nWould you like to do another tax calculation?  Please enter 'y' for yes or 'n' for no.";
+			
+			while 
+			//Present this data nicely formatted.
+			/*
 				Ask the user if they want to do another tax calculation.
 				Accept either "y" or "n" as input.Validate the input and re - do this step if it is not correct.
 				If the user selects "y" start back at step one.If the user selects "n" quit.For all other choices, tell that user that an invalid choice was made and ask them to input again.
